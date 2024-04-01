de26e07eb72ab794b093f6714df1d9362f430dfe
pranga2010 <pranga2010@gmail.com>
2024-04-01 12:39:26 +0530

    New Class Added

---
+++ src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,25 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=98;
+	int b=87;
+	int c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=65465;
+	int b=7;
+	int c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=567865;
+	int b=56;
+	int c=a+b;
+	System.out.println(c);
+}
+}
